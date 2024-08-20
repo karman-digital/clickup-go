cbe17fdd99d4e164ed4ebca72aa3f4dd2ac219af
Jaime Boardman <jaime@karman.digital>
2024-08-20 10:19:28 +0100

    add folderless list function

--- api/lists/methods.go
+++ api/lists/methods.go
@@ -32,3 +32,28 @@ func (ls *ListService) CreateList(folderId string, body listmodels.ListCreationB
 	}
 	return list, nil
 }
+
+func (ls *ListService) CreateFolderlessList(spaceId string, body listmodels.ListCreationBody) (listmodels.List, error) {
+	var list listmodels.List
+	reqBody, err := json.Marshal(body)
+	if err != nil {
+		return listmodels.List{}, err
+	}
+	resp, err := ls.SendRequest(http.MethodPost, "/list", reqBody)
+	if err != nil {
+		return listmodels.List{}, err
+	}
+	defer resp.Body.Close()
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return listmodels.List{}, err
+	}
+	if resp.StatusCode != http.StatusCreated {
+		return listmodels.List{}, err
+	}
+	err = json.Unmarshal(respBody, &list)
+	if err != nil {
+		return listmodels.List{}, err
+	}
+	return list, nil
+}
