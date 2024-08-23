f0fdf3ab959167a07e4b80c29a37fe713579465a
Jaime Boardman <jaime@karman.digital>
2024-08-23 22:14:35 +0100

    add omit empty to non required fields in list post body

--- models/lists/post.go
+++ models/lists/post.go
@@ -2,11 +2,11 @@ package listmodels
 
 type ListCreationBody struct {
 	Name                       string `json:"name"`
-	Content                    string `json:"content"`
-	DueDate                    int64  `json:"due_date"`
-	DueDateTime                bool   `json:"due_date_time"`
-	Priority                   int    `json:"priority"`
-	Assignee                   int    `json:"assignee"`
-	Status                     string `json:"status"`
-	IncludeMarkdownDescription bool   `json:"include_markdown_description"`
+	Content                    string `json:"content,omitempty"`
+	DueDate                    int64  `json:"due_date,omitempty"`
+	DueDateTime                bool   `json:"due_date_time,omitempty"`
+	Priority                   int    `json:"priority,omitempty"`
+	Assignee                   int    `json:"assignee,omitempty"`
+	Status                     string `json:"status,omitempty"`
+	IncludeMarkdownDescription bool   `json:"include_markdown_description,omitempty"`
 }
