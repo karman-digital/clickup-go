9b8cccda9c7e8c6ad82fab52bec915b0c3a0a6be
Jaime Boardman <jaime@karman.digital>
2024-09-25 18:17:53 +0100

    add func to create time entries in cu

--- models/timetracking/postbodies.go
+++ models/timetracking/postbodies.go
@@ -6,10 +6,11 @@ type TimeEntry struct {
 		Name  string `json:"name"`
 		TagBg string `json:"tag_bg"`
 		TagFg string `json:"tag_fg"`
-	} `json:"tags"`
+	} `json:"tags,omitempty"`
 	Start    int64  `json:"start"`
 	Billable bool   `json:"billable"`
 	Duration int64  `json:"duration"`
 	Assignee int    `json:"assignee"`
 	TaskID   string `json:"tid"`
+	End      int64  `json:"end,omitempty"`
 }
